2a8cdf47ccbf18711f613858d46b0fa26fdcccd6
hieuho <thienhieuho@hotmail.com>
2018-03-31 01:36:30 +0700

    Finished project

--- js/main.js
+++ js/main.js
@@ -1,48 +1,6 @@
-var mouseEnter1 = false;
-var mouseEnter2 = false;
-
-$(".hover-dropdown").hover(
-  function() {
-    var target = $(this).attr('aria-controls');
-    console.log('target: ' + target);
-    $('#' + target).collapse('show');
-  }, function() {
-    var curElement = this;
-    setTimeout(function(){
-      var target = $(curElement).attr('aria-controls');
-      var isUsing = $(curElement).attr('data-using');
-      console.log('target: ' + target + '| isUsing: '+ isUsing);
-      if(isUsing != 'true') {
-        $('#' + target).collapse('hide');
-      }
-    }, 200);
-  });
-
-
-function mouseOverHandler(e) {
-  console.log('mouse over');
-  var id = $(e).attr('id');
-  console.log('id: ' + id);
-  var target = $('.hover-dropdown[aria-controls=' + id + ']');
-  $(target).attr('data-using', true);
+function goToPage(url) {
+  window.location.replace(url);
 }
-
-function mouseOutHandler(e) {
-  console.log('mouse out');
-  var id = $(e).attr('id');
-  console.log('id: ' + id);
-  var target = $('.hover-dropdown[aria-controls=' + id + ']');
-  $(target).attr('data-using', false);
-  $(e).collapse('hide');
-}
-
-$('.hd-sub-menu')
-  .mouseover(function() {
-    mouseOverHandler(this);
-  })
-  .mouseleave(function() {
-    mouseOutHandler(this);
-  });
 
 $('li.first-level > a').click(function(e) {
   var parent = $(this).closest('li.first-level');
@@ -61,22 +19,3 @@ $('li.first-level > a').click(function(e) {
     $(parent).toggleClass('open');
   }
 });
-
-
-
-
-/*$('li.first-level > a').click(function(e) {
-  var parent = $(this).closest('li.first-level');
-  $('li.first-level.open').removeClass('open').find('a').click();
-  var isOpen = $(parent).hasClass('open');
-  var target = $(parent).find('ul.second-level');
-  if(target.length > 0) {
-    $(target).toggle();
-    if(isOpen == true) {
-      $(parent).find('.fa-angle-down').toggleClass("fa-angle-down fa-angle-left");
-    } else {
-      $(parent).find('.fa-angle-left').toggleClass(" fa-angle-down fa-angle-left");
-    }
-    $(parent).toggleClass('open');
-  }
-});*/
